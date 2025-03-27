91a9eb15e90b1368d37b3b813162b856e23baa67
lolalex1 <alex.atanasovski03@gmail.com>
2025-03-27 22:01:09 +0000

    Converting indents to tabs

--- _printf.c
+++ _printf.c
@@ -16,25 +16,25 @@ int _putchar(char c)
 
 int print_number (int n)
 {
-        int count = 0;
-        unsigned int num = 0;
+		int count = 0;
+		unsigned int num = 0;
 
-        if (n < 0)
-        {
-                count += _putchar('-');
-                num = -n;
-        }
-        else
-        {
-                num = n;
-        }
+		if (n < 0)
+		{
+				count += _putchar('-');
+				num = -n;
+		}
+		else
+		{
+				num = n;
+		}
 
-        if (num / 10)
-        {
-                count += print_number (num / 10);
-        }
-        count += _putchar(num % 10 + '0');
-        return (count);
+		if (num / 10)
+		{
+				count += print_number (num / 10);
+		}
+		count += _putchar(num % 10 + '0');
+		return (count);
 }
 /**
  * _printf - prints put the parameters just like printf
